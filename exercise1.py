9798ce83b17aff0c5a4771081c5b8f9ee1174963
Josh Date <jjdate@live.com>
2019-02-11 16:40:32 -0500

    done

--- exercise1.py
+++ exercise1.py
@@ -5,3 +5,22 @@ print(2)
 print(3)
 
 print(2 + 3)
+
+print("Betty's pie shop")
+
+print('Betty\'s pie shop')
+
+print("Hello\t\tworld")
+
+print("1. Hello\n2. World")
+
+print("Hello\n\n\n\n\nGoodbye world")
+
+print("Hello \"world\"")
+
+print("Hello \'world\'")
+
+print("Ada Lovelace lived for {} years".format(1852 - 1815))
+
+# Ada Lovelace lived for 37 years
+
