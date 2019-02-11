f5449a31f9551dec6f1662ba96c800356760649c
Josh Date <jjdate@live.com>
2019-02-11 17:12:26 -0500

    2 is done

---
+++ exercise2.py
@@ -0,0 +1,23 @@
+# Question 1
+
+meal = 55
+
+tip = meal * 0.10 
+
+print("Thanks for the {} dollar tip".format(tip))
+
+
+# Question 2
+
+age = "18"
+print(age)
+
+int_age = int(age)
+
+print(int_age)
+
+print("The Answer is {}".format(45628 * 7839))
+
+print(10 < 20 and 30 < 20) or not (10 == 11)
+
+
