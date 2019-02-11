9ea53287b7f8a8c202dc002e00465115b74165dc
Josh Date <jjdate@live.com>
2019-02-11 17:47:07 -0500

    done

--- exercise3.py
+++ exercise3.py
@@ -1,1 +1,32 @@
 print("What is Your Name")
+
+user_name = input()
+
+print("Hello, {}".format(user_name))
+
+secret_password = 'please'
+
+print("What's the password?")
+
+password_attempt = input()
+
+correct_or_not = (password_attempt == secret_password)
+
+print("That's {}!".format(correct_or_not))
+
+
+print("How Many cookies have been eaten?")
+
+eaten = input()
+
+remaining_cookies = 12 - int(eaten)
+
+print("there are {} cookies left.".format(remaining_cookies))
+
+#Bouns
+
+print("How old are you?")
+
+age = input()
+
+print("lol you are {} years old".format(age))
